7ce58a1a653ac57098fa5a6735980861d58dcf43
Fay Vera <fvera.cavalcante@gmail.com>
2021-04-26 15:59:58 -0400

    commit tree 561a288e0c4507583b2e6a25e8857d7acfe0bb27

--- index.js
+++ index.js
@@ -20,3 +20,70 @@ let allWagesFor = function () {
 
     return payable
 }
+
+let createEmployeeRecord = function(rec){
+    return {
+        firstName: rec[0],
+        familyName: rec[1],
+        title: rec[2],
+        payPerHour: rec[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(employeeRow){
+    return employeeRow.map(function(row){
+        return createEmployeeRecord(row)
+    })
+}
+
+let createTimeInEvent = function(startTime){
+    let [date, hour] = startTime.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+    })
+    return this
+}
+
+let createTimeOutEvent = function(endTime){
+    let [date, hour] = endTime.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date,
+        hour: parseInt(hour, 10)
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(dateSought){
+    let inEvent = this.timeInEvents.find(function(e){
+        return e.date === dateSought
+    })
+    let outEvent = this.timeOutEvents.find(function(e){
+        return e.date === dateSought
+    })
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+let wagesEarnedOnDate = function(dateSought){
+    let rawWage = hoursWorkedOnDate.call(this, dateSought) * this.payPerHour
+    return rawWage
+}
+
+let findEmployeeByFirstName = function(collection, firstNameString){
+    return collection.find(function(rec){
+        return rec.firstName === firstNameString
+    })
+}
+
+
+let calculatePayroll = function(empRec){
+    return empRec.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)    
+}
